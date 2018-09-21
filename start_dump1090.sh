5b8e49836de24080390e4422646e6609b681824d
ADSBexchange.com <34561158+adsbxchange@users.noreply.github.com>
2018-09-20 21:08:20 -0700

    Update start_dump1090.sh

--- start_dump1090.sh
+++ start_dump1090.sh
@@ -3,5 +3,5 @@
 while true
 do
         sleep 30
-        sudo /home/pi/dump109/dump1090 --dcfilter --quiet --modeac --net --enable-agc --lat 33 --lon -111 --write-json /var/run/dump1090
+        sudo /home/pi/dump1090/dump1090 --dcfilter --quiet --modeac --net --enable-agc --lat 33 --lon -111 --write-json /var/run/dump1090
 done
