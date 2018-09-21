ee5c47f0e6e6ab8241bbb892920f4acca098fbd1
ADSBexchange.com <34561158+adsbxchange@users.noreply.github.com>
2018-09-20 21:03:36 -0700

    Create start_dump1090.sh

---
+++ start_dump1090.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true
+do
+        sleep 30
+        sudo /home/pi/dump109/dump1090 --dcfilter --quiet --modeac --net --enable-agc --lat 33 --lon -111 --write-json /var/run/dump1090
+done
